8d96ad664d55b42808ff611b55168a34a5cdb95d
Ceren <Zcerens@users.noreply.github.com>
2021-01-23 20:57:02 +0300

    Add project files.

---
+++ MyDictionary/MyDict.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDict<T>
+    {
+        T[] items;
+        public MyDict()
+        {
+            items = new T[0];
+        }
+
+        public void Add(T item)
+        {
+            T[] tempArr = items;
+            items = new T[items.Length + 1];
+                for (int i = 0; i < tempArr.Length; i++)
+            {
+                items[i] = tempArr[i];
+            }
+            items[items.Length - 1] = item;
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyDict<string> name = new MyDict<string>();
+
+            name.Add("Zc");
+            
+
+
+
+
+        }
+    }
+}
